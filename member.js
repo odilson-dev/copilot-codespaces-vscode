e3f725c480dd496a403f5f713955e5156e353f24
Italis Odilson Woodly <109919684+odilson-dev@users.noreply.github.com>
2024-05-31 15:08:34 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,4 +1,4 @@
-function skillsmember() {
+function skillsMember() {
     var member = document.getElementById("member");
     member.style.display = "block";
     var skills = document.getElementById("skills");
